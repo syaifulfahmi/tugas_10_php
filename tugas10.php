d55cb7593c74f62981506fcd13e7fd74112f89d3
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-13 07:51:08 +0700

    Add files via upload

--- tugas10.php
+++ tugas10.php
@@ -1,25 +1,25 @@
 <?php
-$data = array(1,3,8,2,5,7,4,0);
+$arr = array(1,3,8,2,5,7,4,0);
 
 print("Sebelum Di Sorting :<br>");
-print_r($data);
-$data = bubble_sort($data);
-print("<br><br> Setelah Disorting (Buble Sort) :<br>");
-print_r($data);
+print_r($arr);
+$arr = bubble_sort($arr);
+print("<br> Setelah Disorting (Buble Sort) :<br>");
+print_r($arr);
 
-function bubble_sort($data) {
-  $n = count($data);
+function bubble_sort($arr) {
+  $n = count($arr);
        for ($i = 1; $i < $n; $i++) {
            for ($j = $n - 1; $j >= $i; $j--) {
-               if($data[$j-1] > $data[$j]) {
-                   $tmp = $data[$j - 1];
-                   $data[$j - 1] = $data[$j];
-                   $data[$j] = $tmp;
+               if($arr[$j-1] > $arr[$j]) {
+                   $tmp = $arr[$j - 1];
+                   $arr[$j - 1] = $arr[$j];
+                   $arr[$j] = $tmp;
                }
            }
        }
 
-       return $data;
+       return $arr;
 }
 
 ?>
